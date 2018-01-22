c83ac4e11bc9692d9fd2933b82d10fb64dac9d07
bteng22 <bteng22@gmail.com>
2018-01-22 11:10:44 -0800

    adding tests for window dimension resizing

--- src/components/AppState/app-state.test.js
+++ src/components/AppState/app-state.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import AppState from './index';
 
 describe('AppState', () => {
@@ -7,7 +7,9 @@ describe('AppState', () => {
     expect(props).toEqual({
       appState: {
         routeList: expect.any(Object),
-        vehicleData: []
+        vehicleData: [],
+        width: expect.any(Number),
+        height: expect.any(Number)
       },
       setAppState: expect.any(Function)
     })
@@ -23,5 +25,38 @@ describe('AppState', () => {
         <Child />
       </AppState>
     )
+  });
+
+  it('should update window dimensions upon component did mount', () => {
+    const mockFn = jest.fn();
+    const wrapper = shallow(<AppState />);
+    wrapper.instance().updateWindowDimensions = mockFn;
+    wrapper.instance().componentDidMount();
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  it('should register event listener upon component did mount', () => {
+    window.addEventListener = jest.fn();
+    const wrapper = shallow(<AppState />);
+    wrapper.instance().componentDidMount();
+    expect(window.addEventListener).toHaveBeenCalledWith('resize', expect.any(Function));
+  });
+
+  it('should remove event listener upon component will unmount', () => {
+    window.removeEventListener = jest.fn();
+    const wrapper = shallow(<AppState />);
+    wrapper.unmount()
+    expect(window.removeEventListener).toHaveBeenCalledWith('resize', expect.any(Function));
+  });
+
+  it('should update window dimensions', () => {
+    const wrapper = shallow(<AppState />);
+    expect(wrapper.state('width')).toEqual(1024);
+    expect(wrapper.state('height')).toEqual(768);
+    window.innerWidth = 123;
+    window.innerHeight = 321;
+    wrapper.instance().updateWindowDimensions();
+    expect(wrapper.state('width')).toEqual(123);
+    expect(wrapper.state('height')).toEqual(321);
   });
 });
