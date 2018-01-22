d0be26142015e156d2c5c7a8f608a4950ff95ed8
bteng22 <bteng22@gmail.com>
2018-01-22 14:17:26 -0800

    updating README and last touchups

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,12 @@ const App = (props) => {
   const { appState, setAppState } = props;
   return (
     <div className='container'>
+      <div className='title-container'>
+        <h1>MARVELOUS MUNIs</h1>
+        <span>
+          <span role='img' aria-label='emoji'>💻</span> : Brandon Teng
+        </span>
+      </div>
       <RouteControl appState={appState} setAppState={setAppState} />
       <SVGContainer appState={appState} setAppState={setAppState} />
     </div>
