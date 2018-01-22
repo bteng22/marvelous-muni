296c48849fce8026b97a9c057c8b9b67b0304bb6
bteng22 <bteng22@gmail.com>
2018-01-22 13:13:27 -0800

    should update basemap only if dimensions change once

--- src/components/BaseMap/base-map.test.js
+++ src/components/BaseMap/base-map.test.js
@@ -8,8 +8,46 @@ jest.genMockFromModule('../../geojson/streets.json')
 jest.genMockFromModule('../../geojson/freeways.json')
 
 describe('BaseMap', () => {
+  const props = {
+    appState: {
+      width: 123,
+      height: 321
+    }
+  };
   it('should render successfully', () => {
-    const wrapper = shallow(<BaseMap />);
+    const wrapper = shallow(<BaseMap {...props} />);
     expect(wrapper.children().length).toBe(4);
   });
+
+  it('should prepare to update component if window dimensions change', () => {
+    const wrapper = shallow(<BaseMap {...props} />);
+    expect(wrapper.state('shouldUpdate')).toBe(false);
+    const nextProps = {
+      appState: {
+        width: 456,
+        height: 654
+      }
+    }
+    const result = wrapper.instance().checkDimensions(nextProps);
+    wrapper.update();
+    expect(wrapper.state('shouldUpdate')).toBe(true);
+    expect(result).toBe(false);
+  });
+
+  it('should update components if shouldUpdate is true', () => {
+    const nextProps = {
+      appState: {
+        width: 456,
+        height: 654
+      }
+    }
+    const wrapper = shallow(<BaseMap {...props} />);
+    wrapper.setState({ shouldUpdate: true });
+    wrapper.update();
+    const result = wrapper.instance().checkDimensions(nextProps);
+    wrapper.update();
+    expect(wrapper.state('shouldUpdate')).toBe(false);
+    expect(result).toBe(true);
+
+  })
 });
--- src/components/BaseMap/index.js
+++ src/components/BaseMap/index.js
@@ -1,11 +1,31 @@
-import React, { PureComponent, Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import PathGenerator from '../PathGenerator';
 import neighborhoodsJSON from '../../geojson/neighborhoods.json';
 import arteriesJSON from '../../geojson/arteries.json';
 import streetsJSON from '../../geojson/streets.json';
 import freewaysJSON from '../../geojson/freeways.json';
 
-export default class BaseMap extends PureComponent {
+export default class BaseMap extends Component {
+  state = { shouldUpdate: false }
+
+  checkDimensions(nextProps) {
+    const { width: nextWidth, height: nextHeight } = nextProps.appState;
+    const { width, height } = this.props.appState;
+
+    if (this.state.shouldUpdate) {
+      this.setState({ shouldUpdate: false });
+      return true;
+    } else if (nextWidth !== width || nextHeight !== height) {
+      this.setState({ shouldUpdate: true })
+    }
+
+    return false;
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.checkDimensions(nextProps);
+  }
+  
   render() {
     return (
       <Fragment>
